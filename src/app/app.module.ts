c329e7cb882a0e9c86598a35b0170547826b6a8c
Roshan <roshanbhgt@gmail.com>
2020-03-29 22:40:04 +0530

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ModalModule } from 'ngx-bootstrap/modal';
+
 import { AppComponent } from './app.component';
 import { AboutComponent } from './component/about/about.component';
 import { ContactComponent } from './component/contact/contact.component';
 import { BlogComponent } from './component/blog/blog.component';
 import { ContactService } from './Services/contact.service';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -16,10 +21,14 @@ import { ContactService } from './Services/contact.service';
   ],
   imports: [
     BrowserModule,
+    TooltipModule.forRoot(),
+    ModalModule.forRoot(),
+    NgbModule.forRoot()
   ],
   providers: [
       ContactService
   ],
+  exports: [BsDropdownModule, TooltipModule, ModalModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
